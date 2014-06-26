cb2ed0c980d7bd1ad6730f2812f457d4a00c3f98
Longgeek <longgeek@thstack.com>
2014-06-26 18:52:00 +0800

    Add Rakefile file and update puppet coding style
    Add Rakefile file
    Add class document
    Replace the double quotes
    
    Change-Id: I3f38ff0dc150c0cb260e0eb39655fcc351221bd6
    Closes-Bug: #1334603

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,14 @@
+# == Class: apparmor
+#
+# This is the apparmor module. It simply manages the apparmor service.
+#
+
 class apparmor {
-  package { "apparmor":
-    ensure => present
+  package { 'apparmor':
+    ensure => present,
   }
-  service { "apparmor":
-    ensure => 'running';
+
+  service { 'apparmor':
+    ensure => running,
   }
 }
